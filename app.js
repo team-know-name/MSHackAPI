3526367b879ef74741ce331d20324183290b0de4
vaibagga <vaibagga@gmail.com>
2019-12-22 02:50:49 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const app = express();
+const fetch = require("node-fetch");
+const geocoder = require("geocoder");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { PythonShell } = require("python-shell");
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Works");
+});
+app.get("/map_get", (req, res) => {
+  fetch(
+    `https://apis.mapmyindia.com/advancedmaps/v1/fxs1vleongo2371f3mcb4jsjn21ii73x/route_adv/driving/77.2333,28.6665;77.3178,28.4089?steps=true&alternatives=true`
+  )
+    .then(res => res.json())
+    .then(data => {
+      let size = Object.keys(data.routes).length;
+      let mainArray = [];
+
+      for (let i = 0; i < size; i++) {
+        // mainArray.push(data.routes[i].legs[0].steps[j]);
+        let tempArray = [];
+        for (let j = 0; j < data.routes[i].legs[0].steps.length; j++) {
+          tempArray.push(
+            data.routes[i].legs[0].steps[j].intersections[0].location
+          );
+        }
+        mainArray.push(tempArray);
+      }
+
+      let options = {
+        args: mainArray
+      };
+      PythonShell.run("model.py", options, (err, result) => {
+        if (err) {
+          throw err;
+        }
+        res.send(result);
+      });
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
+app.post("/map", (req, res) => {
+  const { start_lat, start_long, end_lat, end_long } = req.body;
+  console.log(start_lat);
+
+  fetch(
+    `https://apis.mapmyindia.com/advancedmaps/v1/fxs1vleongo2371f3mcb4jsjn21ii73x/route_adv/driving/${start_long},${start_lat};${end_lat},${end_long}?steps=true`
+  )
+    .then(res => res.json())
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
+
+app.post("/geocode", (req, res) => {
+  const { location } = req.body;
+  geocoder.geocode(`location`, function(err, data) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(data);
+    }
+  });
+});
+
+app.listen(3344, () => {
+  console.log("Server started on http://localhost:3344");
+});
---
+++ model.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import pickle
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics.pairwise import euclidean_distances
+import sys
+
+class WeightOfPath:
+    def __init__(self, weight_path = "model.pickle"):
+        self.kmeans = pickle.load(open(weight_path, "rb"))
+
+    def pathSum(self, route):
+        pred = self.kmeans.predict(route)
+        pred = self.kmeans.cluster_centers_[pred]
+        return np.sqrt(np.sum((pred - route) ** 2, axis=1))
+
+def main():
+    routes = sys.argv[1:]
+    #print(routes)
+    #route = ['77.233278,28.666487', '77.232199,28.668742', '77.229976,28.676197', '77.233886,28.667097', '77.239048,28.662151', '77.252168,28.650419', '77.255688,28.645179', '77.247154,28.634789', '77.247065,28.6324', '77.246911,28.630731', '77.24704,28.625575', '77.25024,28.615336', '77.254689,28.60141', '77.256378,28.596187', '77.264442,28.582857', '77.263546,28.57585', '77.26098,28.574218', '77.258763,28.572684', '77.283446,28.538613', '77.286808,28.532689', '77.298333,28.511609', '77.299774,28.502367', '77.305478,28.477684', '77.305773,28.475012', '77.309398,28.430756', '77.310748,28.416068', '77.316682,28.415944', '77.317265,28.408842', '77.317802,28.408881']
+    for route in routes:
+        #print(route)
+        route_list = list(map(float, route.split(",")))
+        route_list = np.reshape(route_list, (len(route_list)//2, 2))
+        #print(route_list)
+        w = WeightOfPath()
+        print(np.sum(np.exp(-w.pathSum(route_list))*10**30))
+
+if __name__ ==  "__main__":
+    main()
